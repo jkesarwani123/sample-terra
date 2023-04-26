2f428c658edd88b33a90cf6569b476dea2c7c38b
Juhi K <jkesarwani.us@gmail.com>
2023-04-26 16:37:25 -0400

    cart

--- create-instance/create-instance.tf
+++ create-instance/create-instance.tf
@@ -1,13 +1,13 @@
-variable "sample_string"{
-  default=" hello world"
+variable "sample_string" {
+  default =" hello world"
 }
 # sample_string="hello world"
 
-output "sample_string"{
-  value=var.sample_string
+output "sample_string" {
+  value = var.sample_string
 }
 # echo sample_string
 
-output "sample_string1"{
-  value= " the sample string is ${sample_string}"
+output "sample_string1" {
+  value = " the sample string is ${var.sample_string}"
 }
