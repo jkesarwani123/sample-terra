61f91f5aa60ae75ffde7dd112bda075208fe5b56
Juhi K <jkesarwani.us@gmail.com>
2023-04-26 16:26:02 -0400

    cart

--- create-instance/create-instance.tf
+++ create-instance/create-instance.tf
@@ -1,8 +1,9 @@
-resource "aws_instance" "web" {
-  ami           = "ami-0b5a2b5b8f2be4ec2"
-  instance_type = "t3.micro"
-
-  tags = {
-    Name = "HelloWorld"
-  }
+variable "sample_string"{
+  default=" hello world"
 }
+# sample_string="hello world"
+
+output "sample_string"{
+  value=var.sample_string
+}
+# echo sample_string
