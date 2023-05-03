b7af040a5bf03c2b0b534e9857cb0621c4c2f40c
Juhi K <jkesarwani.us@gmail.com>
2023-05-02 20:20:36 -0400

    terra loops

--- data-sources/main.tf
+++ data-sources/main.tf
@@ -1,16 +1,25 @@
 #use count for loop
-resource "null_resource" "fruits"{
-  count = length(var.fruits)
+/*resource "null_resource" "fruits"{
+ count = length(var.fruits)
 
   provisioner "local-exec"{
-    command = "echo fruit name is ${each.key}"
+    command = "echo fruit name is ${var.fruits[count.index]}"
   }
-}
+} */
 
 variable "fruits"{
   default = {
     apple=10
     bananas=100
     oranges=20
+  }
+}
+
+# Use for each loop - maps
+esource "null_resource" "fruits" {
+  for_each = var.fruits
+
+  provisioner "local-exec" {
+    command = "echo fruit name is ${each.key} = ${each.value} "
   }
 }
