0a6376f0341782bad02216a2f6520a8febbadabb
Juhi K <jkesarwani.us@gmail.com>
2023-05-02 20:30:02 -0400

    terra loops

--- data-sources/main.tf
+++ data-sources/main.tf
@@ -23,3 +23,28 @@ resource "null_resource" "fruits" {
     command = "echo fruit name is ${each.key} = ${each.value} "
   }
 }
+
+variable "nested_fruits"{
+  default = {
+    apple= {
+  name  = "apple"
+  count = 10
+}
+bananas= {
+  name = "bananas"
+  count = 200
+}
+oranges={
+  name= "oranges"
+count = 20}
+}
+}
+
+resource "null_resource" "fruits" {
+for_each = var.nested_fruits
+
+provisioner "local-exec" {
+command = "echo fruit name is ${each.key} = ${each.value["count"]} "
+}
+}
+
