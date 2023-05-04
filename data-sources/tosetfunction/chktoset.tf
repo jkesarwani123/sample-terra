f233c75620b3a7d294a2215a14701f869a6ec1bd
Juhi K <jkesarwani.us@gmail.com>
2023-05-04 17:05:48 -0400

    terra loops

---
+++ data-sources/tosetfunction/chktoset.tf
@@ -0,0 +1,10 @@
+variable vegetables{
+  default = ["carrot", "pumpkin", "onion"]
+}
+
+resource "null_resource" "print_veggies"{
+  for_each = toset(var.vegetables)
+  provisioner "local-exec" {
+    command ="vegetables are ${each.key}"
+  }
+}
